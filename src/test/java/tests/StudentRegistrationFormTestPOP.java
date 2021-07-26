a52bed76ea5fbd065e077e118acea7226ab6284a
IIPAIIOBEDNIK <pereponov_alexander@mail.ru>
2021-07-26 18:55:25 +0300

    Renamed package

--- src/test/java/tests/StudentRegistrationFormTestPOP.java
+++ src/test/java/tests/StudentRegistrationFormTestPOP.java
@@ -1,17 +1,12 @@
-package withPOP;
+package tests;
 
 
 import com.codeborne.selenide.Configuration;
 import com.github.javafaker.Faker;
-import components.Calendar;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
-import java.io.File;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static data.TestData.*;
